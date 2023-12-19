3aa22cc1797859ad41b8390923ccc6eb4aeb64ff
Kevin Baluyot <baluyotkevin33@yahoo.com>
2023-12-18 16:34:56 -0800

    finished auth layout with signup and signin

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,5 +1,7 @@
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
+import { Button } from "../ui/button"
 
 const Header = () => {
   return (
@@ -18,7 +20,18 @@ const Header = () => {
             </Link>
 
             <div className="flex w-32 justify-end gap-3">
-                
+              <SignedIn>
+                <UserButton afterSignOutUrl="/" />
+              </SignedIn>
+                <SignedOut>
+                  <Button asChild className='rounded-full' size='lg'>
+                    <Link
+                    href='/sign-in'
+                    >
+                      Login
+                    </Link>
+                  </Button>
+                </SignedOut>
             </div>
 
         </div>
