163d761b9b176c02c3e9b5e3193c6c3538d022cc
Kevin Baluyot <baluyotkevin33@yahoo.com>
2024-02-05 19:29:28 -0800

    event/category actions and eventform

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -55,20 +55,20 @@ export async function POST(req: Request) {
   const eventType = evt.type;
  
   if(eventType === 'user.created') {
-    const { id, email_addresses, image_url, first_name, last_name, username } = evt.data
+    const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-        clerkId: id,
-        email: email_addresses[0].email_address,
-        username: username!,
-        firstName: first_name,
-        lastName: last_name,
-        photo: image_url
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      username: username!,
+      firstName: first_name,
+      lastName: last_name,
+      photo: image_url,
     }
 
-    const newUser = await createUser(user)
+    const newUser = await createUser(user);
 
-    if (newUser) {
+    if(newUser) {
       await clerkClient.users.updateUserMetadata(id, {
         publicMetadata: {
           userId: newUser._id
@@ -76,33 +76,32 @@ export async function POST(req: Request) {
       })
     }
 
-    return NextResponse.json({ message: 'OK', user: newUser})
+    return NextResponse.json({ message: 'OK', user: newUser })
   }
 
-  if(eventType === 'user.updated') {
-    const { id, image_url, first_name, last_name, username } = evt.data
+  if (eventType === 'user.updated') {
+    const {id, image_url, first_name, last_name, username } = evt.data
 
     const user = {
       firstName: first_name,
       lastName: last_name,
       username: username!,
-      photo: image_url
+      photo: image_url,
     }
-    
+
     const updatedUser = await updateUser(id, user)
 
-    return NextResponse.json({ message: 'OK', user: updatedUser})
-
+    return NextResponse.json({ message: 'OK', user: updatedUser })
   }
- 
-  if(eventType === 'user.deleted') {
+
+  if (eventType === 'user.deleted') {
     const { id } = evt.data
 
     const deletedUser = await deleteUser(id!)
 
-    return NextResponse.json({ message: 'OK', user: deletedUser})
+    return NextResponse.json({ message: 'OK', user: deletedUser })
   }
-
+ 
   return new Response('', { status: 200 })
 }
  
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,18 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
+
 import './globals.css'
 
-const poppins = Poppins({ subsets: ['latin'], weight: ['400', '500', '600', '700'],
-variable: '--font-poppins' })
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-poppins',
+})
 
 export const metadata: Metadata = {
-  title: 'EventStuff',
-  description: 'Events is a platform for event managements',
+  title: 'EventMeet',
+  description: 'Evently is a platform for event management.',
   icons: {
     icon: '/assets/images/logo.svg'
   }
